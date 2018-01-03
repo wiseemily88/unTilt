417ee9329cd8a11eb6e8a078998548d35761109b
Emily Wise <wise.m.emily@gmail.com>
2018-01-03 15:10:23 -0700

    Add failing test for Admin suer Login

---
+++ spec/features/admin_user_can_login_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "As a registered HR Admin user" do
+ let(:admin) { create(:admin_user)  }
+
+  it "I can log in" do
+    visit root_path
+    click_on 'Login'
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "session[email]", with: "#{admin.email}"
+    fill_in "session[password]", with: "#{admin.password}"
+
+    within(".action") do
+     click_on("Login")
+
+     expect(current_path).to eq(dashboard_index_path)
+    end
+  end
+
+ it "I can log out" do
+   login_user(admin.email, admin.password)
+   click_on 'Logout'
+
+   expect(current_path).to eq(root_path)
+   expect(page).to have_content("Login")
+ end
+
+end
